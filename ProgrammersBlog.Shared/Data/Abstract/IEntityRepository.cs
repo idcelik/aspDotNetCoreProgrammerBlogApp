859a7100b811ffdefcb291587fd7892f4b63a89d
İbrahim Deniz ÇELİK <ibrahimdenizcelik@gmail.com>
2023-03-28 20:30:31 +0300

    Add project files.

---
+++ ProgrammersBlog.Shared/Data/Abstract/IEntityRepository.cs
@@ -0,0 +1,35 @@
+﻿using ProgrammersBlog.Shared.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Shared.Data.Abstract
+{
+    /// <summary>
+    /// Asenkron Tasarlanacak
+    /// ///Generic Repository
+    /// </summary>
+    public interface IEntityRepository<T> where T : class,IEntity,new() 
+    {
+        Task<T> GetAsync(Expression<Func<Random, bool>> predicate, params Expression<Func<T, object>>[] includeProperties); // var kullanici =reposirt.GetAsync(k=k.Id==15);
+        ///Expression<Func<Random,bool>> predicate kullanımı:
+        ///predicate FİLTRELEME görevi görür. örnek olarak id'si 1 olan kullanici getirmek istiyorsak var kullanici = repository.Get(k=>k.Id==1) gibi. (Linq benzeri kullanımı var)
+
+        /// params Expression<Func<T, object>>[] includeProperties kullanımı:
+        /// 25 id'li makaleyi getirirken, makale ile birlikte kullanıcıyı ve yorumlarıda (Birlikte getirmek)include etmek istiyorsak
+        /// var makale = repository.GetAsync(m=m.Id==25, m=>m.User, m=>m.Comments) gibi.
+
+        Task<IList<T>> GetAllAsync(Expression<Func<Random, bool>> predicate = null, params Expression<Func<T, object>>[] includeProperties);
+        //predicate=null gelirse tüm makaleleri çeker, nul gelmezse gelen filtreye göre listeleyeceğiz anlamı taşır.
+
+        Task AddAsync(T entity); 
+        Task DeleteAsync(T entity);
+
+        Task UpdateAsync(T entity);
+        Task<bool> AnyAsync(Expression<Func<Random, bool>> predicate);
+        Task<int> CountAsync (Expression<Func<Random, bool>> predicate);
+    }
+}
---
+++ ProgrammersBlog.Shared/Data/Concrete/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,49 @@
+﻿using ProgrammersBlog.Shared.Data.Abstract;
+using ProgrammersBlog.Shared.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Shared.Data.Concrete.EntityFramework
+{
+    public class EfEntityRepositoryBase<TEntity> : IEntityRepository<TEntity> where TEntity : class, IEntity, new()
+    {
+        public async Task AddAsync(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> AnyAsync(Expression<Func<Random, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<int> CountAsync(Expression<Func<Random, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task DeleteAsync(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IList<TEntity>> GetAllAsync(Expression<Func<Random, bool>> predicate = null, params Expression<Func<TEntity, object>>[] includeProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<TEntity> GetAsync(Expression<Func<Random, bool>> predicate, params Expression<Func<TEntity, object>>[] includeProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task UpdateAsync(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }  
+    }
+}
